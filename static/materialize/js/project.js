be91d4f5341c89cb9e4e2b5672ce45ea8937b8e9
Demidov Nikita <demidov.n@northeastern.edu>
2021-03-21 18:06:44 -0400

    Sidebar update
    Added sidebar with many options

--- static/materialize/js/project.js
+++ static/materialize/js/project.js
@@ -1,7 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {
-            coverTrigger: false,
-            constrainWidth: false
-        })
-  });
+    document.addEventListener('DOMContentLoaded', function() {
+        var elem = document.querySelector('.sidenav');
+        var collapsibleInstance = M.Sidenav.init(elem, {});
+        var collapsibleElem = document.querySelector('.collapsible');
+        var collapsibleInstance = M.Collapsible.init(collapsibleElem);
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        var instances = M.Dropdown.init(elems, {
+                hover: true,
+                coverTrigger: false,
+                constrainWidth: true
+                });
+            });
